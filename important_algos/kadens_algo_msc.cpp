8fb8e94191499b001e66b70254bf20b5b3221e6c
Prakash Choudhary <prakash15122000@gmail.com>
2020-10-28 20:23:34 +0530

    kadens algorithm added

---
+++ important_algos/kadens_algo_msc.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxsum(vector<int> vect){
+
+    int max = vect[0];
+    for (int i = 0; i < vect.size(); i++)
+    {
+        if(vect[i]>max) max = vect[i];
+    }
+
+    if(max<0) return max;
+
+    int sum = 0;
+    int ans = vect[0];
+
+    for(int i=0;i<vect.size();i++){
+        if(sum+vect[i]>0){
+            sum+=vect[i];
+            if(sum>ans) ans = sum;
+        }
+
+        else
+        {
+            sum = 0;
+        }
+        
+    }
+
+    return sum;
+    
+}
+
+int main(){
+    int size;
+    cout<<"enter the size of array"<<endl;
+    cin>>size;
+
+    vector<int> V;
+
+    cout<<"enter "<<size<<" number of integers for array"<<endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        int x;
+        cin>>x;
+        V.push_back(x);
+    }
+
+    int sum = maxsum(V);
+
+    cout<<"maximum sum is: "<<sum<<endl;
+    
+
+    return 0;
+}
